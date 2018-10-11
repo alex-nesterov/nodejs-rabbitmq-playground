9fc699ced45f15d37eebca4cad71fa13791953f0
Alex Nesterov <alex@nesterov.email>
2018-10-11 16:02:55 +0300

    added command params and empty lines between command groups at package.json, fixed bug at receive_logs_topic

--- src/rabbit-topics/receive_logs_topic.js
+++ src/rabbit-topics/receive_logs_topic.js
@@ -22,12 +22,12 @@ if (args.length === 0) {
 
     const channel = await connection.createChannel();
     const ex = 'topic_logs';
-    const exchange = channel.assertExchange(ex, 'topic', { durable: false });
+    channel.assertExchange(ex, 'topic', { durable: false });
     const queue = channel.assertQueue('', { exclusive: true });
 
     console.log(' [*] Waiting for logs. To exit press CTRL+C');
     args.forEach((key) => {
-      channel.bindQueue(queue.queue, exchange, key);
+      channel.bindQueue(queue.queue, ex, key);
     });
 
     channel.consume(queue.queue, (msg) => {
